b77b468cbfd08dbe42ef5513f3c32c2e7ba64c99
Ajay Dattu Naikwade <ajaydnaikwade15@gmail.com>
2026-06-25 19:36:22 +0530

    Complete Task 2: Implement data model

---
+++ src/main/java/com/wellsfargo/counselor/entity/Portfolio.java
@@ -0,0 +1,52 @@
+package com.wellsfargo.counselor.entity;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+
+@Entity
+public class Portfolio {
+
+    @Id
+    @GeneratedValue()
+    private long portfolioId;
+
+    @Column(nullable = false)
+    private String creationDate;
+
+    @ManyToOne
+    @JoinColumn(name = "client_id", nullable = false)
+    private Client client;
+
+    protected Portfolio() {
+
+    }
+
+    public Portfolio(String creationDate, Client client) {
+        this.creationDate = creationDate;
+        this.client = client;
+    }
+
+    public Long getPortfolioId() {
+        return portfolioId;
+    }
+
+    public String getCreationDate() {
+        return creationDate;
+    }
+
+    public void setCreationDate(String creationDate) {
+        this.creationDate = creationDate;
+    }
+
+    public Client getClient() {
+        return client;
+    }
+
+    public void setClient(Client client) {
+        this.client = client;
+    }
+}
